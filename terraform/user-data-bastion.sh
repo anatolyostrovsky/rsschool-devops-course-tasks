749e9ffb01b321e7944924f06633eeebc6bf5e0d
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2024-12-01 18:59:18 +0000

    task7 update

---
+++ terraform/user-data-bastion.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+curl -sfL https://get.k3s.io | sh -
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+echo "k3s server installed sucessfully"
+
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+echo "Helm installed successfully"
+
+sudo yum install git -y
+sudo git clone https://github.com/anatolyostrovsky/wp-helm-chart.git
+echo "Git installed and chart repository copied"
+
+cd wp-helm-chart
+sudo su
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+helm install wordpress wordpress-chart
+echo "Wordpress service is now live and available on port 30080"
+
+cd ..
+kubectl create namespace monitoring
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+echo "server:
+  service:
+    type: NodePort
+    nodePort: 30099
+
+additionalScrapeConfigs:
+  - job_name: 'mysql'
+    static_configs:
+      - targets: ['mysql-exporter-prometheus-mysql-exporter:9104']
+
+extraScrapeConfigsSecret:
+  enabled: true
+
+nodeExporter:
+  enabled: true
+
+mysql-exporter-prometheus-mysql-exporter:
+  enabled: true
+  service:
+    port: 9104" > values.yaml
+
+helm install mysql-exporter prometheus-community/prometheus-mysql-exporter --namespace monitoring
+    
+helm install prometheus prometheus-community/prometheus --namespace monitoring -f values.yaml
+echo "Prometheus installed and running on port 30099"
+
+    
+
